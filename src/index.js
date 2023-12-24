e258c3abb052331a4aed00c944c807fd0a7263fe
Praveen Kumar Padhi <93372387+praveenkupadhi@users.noreply.github.com>
2023-12-25 01:57:29 +0530

    refactor: format

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import { store } from "./redux/store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './redux/store';
 
 ReactDOM.render(
-	<BrowserRouter>
-		<Provider store={store}>
-			<React.StrictMode>
-				<App />
-			</React.StrictMode>
-		</Provider>
-	</BrowserRouter>,
-	document.getElementById("root")
+  <BrowserRouter>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
